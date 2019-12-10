35bb90fd7fb50dd9658249efbf0fae6b8100803e
lucikluc <lucka.mandova@gmail.com>
2019-12-10 18:36:29 +0100

    Void functions from Tulko

--- src/Cat.java
+++ src/Cat.java
@@ -5,5 +5,9 @@ public class Cat extends Pets{
 		super(n,ec,br,f,s,ag);
 	}
 	
+	public void printCats() {
+		System.out.println("Name:" + getName() + " " + "Color of eyes: " + getEyeColor() + " " + "Breed: " + getBreed() + " " + "Color of fur: " + getFur() + " " + "Sex: " + getSex() + " " + "Age: " + getAge() );
+	
+	}
 	
 }
--- src/Dog.java
+++ src/Dog.java
@@ -4,4 +4,14 @@ public class Dog extends Pets{
 	public Dog(String n,String ec,String br,String f,String s,int ag) {
 		super(n,ec,br,f,s,ag);
 	}
+	
+	public void printInfoD() {
+		System.out.print(" "+ getName());
+	}
+	
+	public void printDogs() {
+		System.out.println("Name:" + getName() + " " + "Color of eyes: " + getEyeColor() + " " + "Breed: " + getBreed() + " " + "Color of fur: " + getFur() + " " + "Sex: " + getSex() + " " + "Age: " + getAge() );
+	}
+	
+
 }
